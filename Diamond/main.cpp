728e962f2f84d1b24cf0789c75922125490e4539
Ilya <ischetverikov@gmail.com>
2019-02-11 19:33:31 +0300

    Changes after passing assignment

--- Diamond/main.cpp
+++ Diamond/main.cpp
@@ -5,32 +5,38 @@ using namespace std;
 
 class A {
 public:
-
+	A() { cout << "A"; };
+	A(const A &) { cout << "a"; };
+	void virtual fun() { cout << "gun " << endl; }
 	void virtual fun1(int m) { cout << "fun A" << endl; }
 };
 class B : public virtual A {
 public:
-	
-	void fun()  { cout << "fun B" << endl; }
+	B() { cout << "B"; };
+	B(const B &) { cout << "b"; };
+	//void fun()  { cout << "fun B" << endl; }
 };
 class C : public virtual A {
 public:
-	
-	void fun()  { cout << "fun C" << endl; }
+	C() { cout << "C"; };
+	C(const C &) { cout << "c"; };
+	//void fun()  { cout << "fun C" << endl; }
 };
-class D : public virtual B, public virtual C {
+class D : B, C {
 public:
-	
+	D() { cout << "D"; };
+	D(const D &) { cout << "d"; };
 	//void fun()  { cout << "fun D" << endl; }
 };
 void test() {
-	A a; 	B b; 	C c; 	D d;
-	a.fun1(5); b.fun(); c.fun(); d.fun1(4);
+	//A a; 	B b; 	C c; 	D d;
+	//a.fun1(5); b.fun(); c.fun(); d.::B::fun();
 }
 
 int main() {
 
-	test();
-
+	//test();
+	D d1;
+	D d2(d1);
 	return 0;
 }
